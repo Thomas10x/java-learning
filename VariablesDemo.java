b27a02ab5d1e2738043c402a50a24de303bd20e2
Thomas Mathew <thomasmathew@bec.org>
2026-02-02 14:22:53 +0530

    Learning about Variables

---
+++ VariablesDemo.java
@@ -0,0 +1,16 @@
+public class VariablesDemo {
+    public static void main (String [] args) {
+        int age = 10;
+        double weight = 10.44;
+        char label = 'A';
+        boolean isFun = true;
+        String name = "Thomas";
+
+        System.out.println("Age is " + age);
+        System.out.println("weight " + weight);
+        System.out.println("char " + label);
+        System.out.println("isFun " + isFun);
+        System.out.println("name " + name);
+
+    }
+}
